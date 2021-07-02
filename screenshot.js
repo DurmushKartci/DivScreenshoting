966baa88d5d761ad264dd50bc11c398f03b43af1
Durmuş Kartcı <67950050+DurmushKartci@users.noreply.github.com>
2021-07-02 21:27:42 +0300

    Added a chart using chart.js in a div

--- screenshot.js
+++ screenshot.js
@@ -21,6 +21,42 @@ muhammedbutton.onclick = function() {
     divName = "Muhammed Fatih Kartcı";
 }
 
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
 
 async function doCapture() {
 
@@ -38,20 +74,5 @@ async function doCapture() {
         document.body.appendChild(link)
         link.click()
         document.body.removeChild(link)
-
-        //     saveAs(canvas.doDataURL("image/png", 0.9), "image.png");
-
-        //     // console.log(canvas.toDataURL("image/jpeg", 0.9));
-        //     // var ajax = new XMLHttpRequest();
-
-        //     // ajax.open("POST", "save-capture.php", true);
-        //     // ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-        //     // ajax.send("image=" + canvas.toDataURL("image/jpeg", 0.9));
-
-        //     // ajax.onreadystatechange = function() {
-        //     //     if (this.readyState == 4 && this.status == 200) {
-        //     //         console.log(this.responseText)
-        //     //     }
-        //     // }
     });
 }
